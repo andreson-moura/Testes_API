b0247fdaa1ba4b942c745bceaf4116b3ebd4115c
Andreson Moura <58272501+andreson-moura@users.noreply.github.com>
2023-09-09 17:59:40 -0400

    add api intelltech

---
+++ Desafio/Data/AppDbContext.cs.cs
@@ -0,0 +1,11 @@
+﻿using Desafio;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ Desafio/Models/UserController.cs.cs
@@ -0,0 +1,57 @@
+﻿using Desafio;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/users")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] User user)
+    {
+        if (string.IsNullOrEmpty(user.Name) || string.IsNullOrEmpty(user.Email))
+        {
+            return BadRequest("Nome e email são obrigatórios.");
+        }
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+        return Ok(user);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] User updatedUser)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Email = updatedUser.Email;
+        _context.SaveChanges();
+        return Ok(user);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        _context.SaveChanges();
+        return NoContent();
+    }
+}
